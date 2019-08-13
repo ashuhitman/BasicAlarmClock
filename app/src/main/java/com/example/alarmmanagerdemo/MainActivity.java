2f526dac01ecc4aa64a14c5a2aeac58c42a59604
ashuhitman <ashu8306@gmail.com>
2019-08-13 18:17:03 +0530

    DatePicker DialogBox using DialogFragment

--- app/src/main/java/com/example/alarmmanagerdemo/MainActivity.java
+++ app/src/main/java/com/example/alarmmanagerdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alarmmanagerdemo;
 
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -8,16 +9,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements DemoDialog.DemoDialogListener {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    private TextView mTextViewUsername;
-    private TextView mTextViewPassword;
+    private TextView mTextViewDate;
+
     private Button mButton;
 
     @Override
@@ -25,27 +28,29 @@ public class MainActivity extends AppCompatActivity implements DemoDialog.DemoDi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextViewUsername = findViewById(R.id.username_textview);
-        mTextViewPassword = findViewById(R.id.textview_password);
+        mTextViewDate = findViewById(R.id.date_textview);
         mButton = findViewById(R.id.open_dialog_button);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDialog();
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(getSupportFragmentManager(), "Date Picker");
             }
         });
 
 
     }
 
-    private void openDialog() {
-        DemoDialog dialog = new DemoDialog();
-        dialog.show(getSupportFragmentManager(), "Demo Dialog");
-    }
+
 
     @Override
-    public void applyTexts(String username, String password) {
-        mTextViewUsername.setText(username);
-        mTextViewPassword.setText(password);
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance().format(c.getTime());
+        mTextViewDate.setText(currentDateString);
+
     }
 }
