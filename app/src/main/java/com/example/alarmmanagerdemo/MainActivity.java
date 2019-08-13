554388fc168fb874fe5036a041eb3723216701fa
ashuhitman <ashu8306@gmail.com>
2019-08-13 17:47:06 +0530

    Custom DialogBox -> DialogFragment

--- app/src/main/java/com/example/alarmmanagerdemo/MainActivity.java
+++ app/src/main/java/com/example/alarmmanagerdemo/MainActivity.java
@@ -7,48 +7,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DemoDialog.DemoDialogListener {
 
-    TimePicker mTimePicker;
+    private TextView mTextViewUsername;
+    private TextView mTextViewPassword;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTimePicker = findViewById(R.id.time_picker);
-
-        findViewById(R.id.set_alarm_button).setOnClickListener(new View.OnClickListener() {
+        mTextViewUsername = findViewById(R.id.username_textview);
+        mTextViewPassword = findViewById(R.id.textview_password);
+        mButton = findViewById(R.id.open_dialog_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // convert time into milliseconds
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(
-                        calendar.get(Calendar.YEAR),
-                        calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH),
-                        mTimePicker.getHour(),
-                        mTimePicker.getMinute(),
-                        0
-                );
-
-                setAlarm(calendar.getTimeInMillis());
+                openDialog();
             }
         });
+
+
     }
 
-    private void setAlarm(long timeInMillis) {
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, MyAlarm.class);
+    private void openDialog() {
+        DemoDialog dialog = new DemoDialog();
+        dialog.show(getSupportFragmentManager(), "Demo Dialog");
+    }
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
-        alarmManager.setRepeating(AlarmManager.RTC, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
-
-        Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show();
+    @Override
+    public void applyTexts(String username, String password) {
+        mTextViewUsername.setText(username);
+        mTextViewPassword.setText(password);
     }
 }
